7dccf1e59758ede936add214e1f3e3f098fe8d7f
Sebastien Cadorette <cado.94@me.com>
2019-10-29 21:48:48 -0400

    `GET members` route.

--- routes/rest/members.js
+++ routes/rest/members.js
@@ -1,9 +1,21 @@
 const express = require('express')
 
 module.exports = serviceTeam => {
-  const router = express.Router()
+	const router = express.Router()
 
-  // À COMPLÉTER
+	router.get('/', (req, res, next) => {
+		serviceTeam.getTeamMembers((err, data) => {
+			if (err) {
+				if (req.app.locals.t && req.app.locals.t['ERRORS'] && req.app.locals.t['ERRORS']['MEMBERS_ERROR']) {
+					res.status(500).json({ 'errors': [req.app.locals.t['ERRORS']['MEMBERS_ERROR']] });
+				} else {
+					res.status(500).json({ 'errors': [err.message] });
+				}
+			} else {
+				res.json(data);
+			}
+		});
+	});
 
-  return router
+	return router
 }
