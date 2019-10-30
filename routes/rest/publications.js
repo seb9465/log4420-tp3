1f343dbd3437660119a88805fdca55831f4a877d
Sebastien Cadorette <cado.94@me.com>
2019-10-29 22:28:44 -0400

    GET api/publications/

--- routes/rest/publications.js
+++ routes/rest/publications.js
@@ -2,9 +2,37 @@ const express = require('express')
 const async = require('async')
 
 module.exports = servicePublication => {
-  const router = express.Router()
+	const router = express.Router()
 
-  // À COMPLÉTER
+	router.get('/', (req, res, next) => {
+		const sortByOptions = { date: 'date', title: 'title' };
+		const orderByOptions = { asc: 'asc', desc: 'desc' };
 
-  return router
+		const limit = req.params && req.param.limit ? req.params.limit : 10;
+		const page = req.params && req.param.page ? req.params.page : 1;
+		const sortBy = req.params && req.param.sort_by ? req.params.sort_by : sortByOptions.date;
+		const orderBy = req.params && req.params.order_by ? req.params.order_by : orderByOptions.desc;
+		const pageOpts = {
+			limit: limit,
+			pageNumber: page,
+			sorting: [
+				[ sortBy, orderBy ]
+			]
+		};
+
+		servicePublication.getPublications(pageOpts)((err, data) => {
+			if (err) {
+				if (req.app.locals.t && req.app.locals.t['ERRORS'] && req.app.locals.t['ERRORS']['PUBS_ERROR']) {
+					res.status(500).json({ 'errors': [req.app.locals.t['ERRORS']['PUBS_ERROR']] });
+				} else {
+					res.status(500).json({ 'errors': [err.message] });
+				}
+			} else {
+				data = data ? data : [];
+				res.json({ 'count': data.length, 'publications': data});
+			}
+		});
+	});
+
+	return router
 }
