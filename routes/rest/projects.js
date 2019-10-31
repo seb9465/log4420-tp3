58ad0af39a936fd278e17c4a7fd006ca6415f282
Sebastien Cadorette <cado.94@me.com>
2019-10-30 20:32:16 -0400

    GET projects done

--- routes/rest/projects.js
+++ routes/rest/projects.js
@@ -2,9 +2,19 @@ const express = require('express')
 const async = require('async')
 
 module.exports = (serviceProjects, servicePublication) => {
-  const router = express.Router()
+	const router = express.Router()
 
-  // À COMPLÉTER
+	router.get('/', (req, res, next) => {
+		serviceProjects.getProjects(req.app.locals.lang)((err, data) => {
+			if (err) {
+				res.status(500).json({ 'errors': [err.message] });
+			} else {
+				res.status(200).json(data);
+			}
+		});
+	});
 
-  return router
+	
+
+	return router
 }
