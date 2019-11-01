9a416be55bcb8677126020a5de3a8223b3ae330b
Sebastien Cadorette <cado.94@me.com>
2019-11-01 15:40:16 -0400

    route pour la vue de `team`.

--- routes/team.js
+++ routes/team.js
@@ -1,6 +1,19 @@
 const express = require('express')
 const router = express.Router()
+const fetch = require('node-fetch')
 
-// À COMPLÉTER
+router.get('/', (req, res, next) => {
+	fetch('http://localhost:3000/api/members/')
+		.then(response => response.json())
+		.then(members => {
+			res.render('./../views/team', { members: members }, (err, html) => {
+				if (err) {
+					throw err;
+				} else {
+					res.send(html);
+				}
+			});
+		});
+});
 
 module.exports = router
