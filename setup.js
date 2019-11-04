b384035653dd07381dc1fe8a3906a3dea77531b6
Sebastien Cadorette <cado.94@me.com>
2019-11-04 08:51:30 -0500

    Ajustement version MongoDb & test

--- setup.js
+++ setup.js
@@ -3,3 +3,26 @@ const yaml = require('js-yaml')
 const fs = require('fs')
 const config = require('./config.json')
 
+let db;
+
+MongoClient.connect(config.dbUrl + '/' + config.dbName, (err, d) => {
+	if (err) throw err;
+	db = d;
+
+	var collection = db.collection('foods')
+	collection.insert({ name: 'taco', tasty: true }, function (err, result) {console.log('FOOD')});
+
+	deleteAllCollections();
+});
+
+function deleteAllCollections () {
+	db.listCollections().toArray((err, coll) => {
+		coll.forEach(collection => {
+			db.collection(collection.name).drop((err, delOk) => {
+				if (err) throw err;
+				if (delOk) console.log(`[MONGO_DB] Droped collection \'${collection.name}\'`);
+			});
+		});
+		console.log('after foreach');
+	});
+}
