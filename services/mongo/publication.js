d5765de552511b3567f5ee6276a0e430668bfcc5
Sebastien Cadorette <cado.94@me.com>
2019-11-13 16:30:03 -0500

    format document

--- services/mongo/publication.js
+++ services/mongo/publication.js
@@ -16,8 +16,8 @@ const mongodb = require('mongodb')
  *  @param {numPublicationsCallback} callback - Fonction de rappel pour obtenir le résultat
  */
 const getNumberOfPublications = db => callback => {
-  // À COMPLÉTER
-  callback(null, 0)
+
+	callback(null, 0)
 }
 
 /**
@@ -42,8 +42,8 @@ const getNumberOfPublications = db => callback => {
  *  @param {publicationsCallback} callback - Fonction de rappel pour obtenir le résultat
  */
 const getPublications = db => pagingOpts => callback => {
-  // À COMPLÉTER
-  callback(null, [])
+	// À COMPLÉTER
+	callback(null, [])
 }
 
 /**
@@ -62,8 +62,8 @@ const getPublications = db => pagingOpts => callback => {
  *  @param {createdPublicationCallback} callback - Fonction de rappel pour obtenir la publication créée
  */
 const createPublication = db => publication => callback => {
-  // À COMPLÉTER
-  callback()
+	// À COMPLÉTER
+	callback()
 }
 
 /**
@@ -74,8 +74,8 @@ const createPublication = db => publication => callback => {
  *  @param callback - Fonction de rappel qui valide la suppression
  */
 const removePublication = db => id => callback => {
-  // À COMPLÉTER
-  callback()
+	// À COMPLÉTER
+	callback()
 }
 
 /**
@@ -94,16 +94,16 @@ const removePublication = db => id => callback => {
  *  @param {projectPublicationsCallback} callback - Fonction de rappel pour obtenir le résultat
  */
 const getPublicationsByIds = db => pubIds => callback => {
-  // À COMPLÉTER
-  callback(null, [])
+	// À COMPLÉTER
+	callback(null, [])
 }
 
 module.exports = db => {
-  return {
-    getPublications: getPublications(db),
-    createPublication: createPublication(db),
-    removePublication: removePublication(db),
-    getPublicationsByIds: getPublicationsByIds(db),
-    getNumberOfPublications: getNumberOfPublications(db)
-  }
+	return {
+		getPublications: getPublications(db),
+		createPublication: createPublication(db),
+		removePublication: removePublication(db),
+		getPublicationsByIds: getPublicationsByIds(db),
+		getNumberOfPublications: getNumberOfPublications(db)
+	}
 }
