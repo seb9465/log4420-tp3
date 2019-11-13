c5c8a7da555af8f63c1bd1c7ed655fea71a9ab6a
Sebastien Cadorette <cado.94@me.com>
2019-11-13 15:15:03 -0500

    fonction pour récupérer les `seminars`.

--- services/mongo/seminar.js
+++ services/mongo/seminar.js
@@ -31,9 +31,69 @@ const getSeminars = db => query => language => callback => {
 			}
 		});
 
-		callback(null, seminars);
+		const transformations = [filterSeminarsByFromDate(query), sortSeminars(query)]
+		const transformedSeminars = transformations.reduce((acc, f) => f(acc), seminars)
+
+		callback(null, transformedSeminars);
 	});
 }
+
+
+/**
+ *  Filtre les séminaires avant une date particulière.
+ *
+ *  Si la date n'est pas fournise, on renvoie le tableau initial.
+ *
+ *  @param {Object} query - Objet avec le champ 'query.from'
+ *  @param {Array} seminars - Tableau de séminaires à filtrer
+ *  @returns {Array} Tableau de séminaires potentiellement filtré
+ */
+const filterSeminarsByFromDate = query => seminars => {
+	if (query !== undefined && query.from !== undefined) {
+	  return seminars.filter(s => s.date > query.from)
+	} else {
+	  return seminars
+	}
+  }
+  
+  /**
+   *  Trie les séminaires selon un champ et un ordre particulier.
+   *
+   *  Si le champ et/ou l'ordre n'est pas fournis, on renvoie le tableau initial.
+   *
+   *  @param {Object} query - Objet avec le champ 'query.sort.field' et 'query.sort.order'
+   *  @param {Array} seminars - Tableau de séminaires à filtrer
+   *  @returns {Array} Tableau de séminaires potentiellement trié
+   */
+  const sortSeminars = query => seminars => {
+	const canSortByField = query !== undefined &&
+	  query.sort !== undefined &&
+	  query.sort.field !== undefined &&
+	  (query.sort.order === 'ASC' || query.sort.order === 'DESC')
+	if (canSortByField) {
+	  return seminars.sort((s1, s2) => compare(query.sort.order)(s1.date)(s2.date))
+	} else {
+	  return seminars
+	}
+  }
+  
+  /**
+   *  Fonction de comparaison entre deux valeurs et un ordre.
+   *
+   *  @param {string} order - Valeurs ASC ou DESC
+   *  @param v1 - Première valeur
+   *  @param v2 - Deuxième valeur
+   *  @returns {int} Valeur de -1, 0 ou 1 selon le paramètre order
+   */
+  const compare = order => v1 => v2 => {
+	if (order === 'ASC') {
+	  return (v1 < v2) ? -1 : v1 > v2 ? 1 : 0
+	} else if (order === 'DESC') {
+	  return (v1 < v2) ? 1 : v1 > v2 ? -1 : 0
+	} else {
+	  return 0
+	}
+  }
 
 module.exports = db => {
 	return {
