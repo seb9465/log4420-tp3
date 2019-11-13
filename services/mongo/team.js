0a4c2ea7f9ac63fcd4d2782a79d8a69f0a778c80
Sebastien Cadorette <cado.94@me.com>
2019-11-13 15:33:51 -0500

    fonction pour récupérer les `members`.

--- services/mongo/team.js
+++ services/mongo/team.js
@@ -7,8 +7,19 @@
  */
 
 const getTeamMembers = db => callback => {
+	db.collection('members').find().toArray((err, dbMembers) => {
+		if (err) throw err;
 
-	callback(null, [])
+		const members = (dbMembers === null ? [] : dbMembers).sort((m1, m2) => {
+			if (m1.lastname === m2.lastname) {
+				return (m1.firstname < m2.firstname) ? -1 : (m1.firstname > m2.firstname) ? 1 : 0
+			} else {
+				return (m1.lastname < m2.lastname) ? -1 : 1
+			}
+		});
+
+		callback(null, members);
+	});
 }
 
 module.exports = db => {
