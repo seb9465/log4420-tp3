c2a58fbc538c91da1c4fa13c52a457e7a86aa6ad
Sebastien Cadorette <cado.94@me.com>
2019-11-16 15:46:21 -0500

    ajustement du POST des publications pour que tous les tests passent

--- routes/rest/publications.js
+++ routes/rest/publications.js
@@ -45,29 +45,31 @@ module.exports = servicePublication => {
 		const errors = []
 
 		// req.body vide
-		if (Object.keys(req.body).length === 0) {
-			errors.push(req.app.locals.t['ERRORS']['PUB_CREATE_ERROR']);
-		} 
-		// Aucun auteur
-		if (req.body.authors === undefined || req.body.authors.length === 0) {
-			errors.push(req.app.locals.t['ERRORS']['AUTHOR_EMPTY_FORM']);
-		} 
-		// année n'est pas un nombre
-		if (typeof req.body.year != 'number') {
-			errors.push(req.app.locals.t['ERRORS']['YEAR_NOT_INT_FORM']);
-		} 
-		// le mois n'est pas entre 0 et 11
-		if (req.body.month < MIN_MONTH || req.body.month > MAX_MONTH) {
-			errors.push(req.app.locals.t['ERRORS']['MONTH_ERROR_FORM']);
-		} 
-		// titre n'a pas au moins 5 caractères
-		if (req.body.title.length < TITLE_MIN_CHARACTER_NUMBER) {
-			errors.push(req.app.locals.t['ERRORS']['PUB_AT_LEAST_5_CHAR_FORM']);
-		} 
-		// Conférence n'a pas au moins 5 caractères
-		if (req.body.venue.length < VENUE_MIN_CHARACTER_NUMBER) {
-			errors.push(req.app.locals.t['ERRORS']['VENUE_AT_LEAST_5_CHAR_FORM']);
-		} 
+
+		if (req.body === undefined || Object.keys(req.body).length === 0) {
+			errors.push(req.app.locals.t['ERRORS']['EMPTY_PUBLICATION_FORM']);
+		} else {
+			// Aucun auteur
+			if (req.body.authors === undefined || req.body.authors.length === 0) {
+				errors.push(req.app.locals.t['ERRORS']['AUTHOR_EMPTY_FORM']);
+			} 
+			// année n'existe pas ou n'est pas un nombre
+			if (req.body.year === undefined || typeof req.body.year != 'number') {
+				errors.push(req.app.locals.t['ERRORS']['YEAR_NOT_INT_FORM']);
+			} 
+			// le mois n'existe pas ou n'est pas un nombre ou n'est pas entre 0 et 11
+			if (req.body.month === undefined || typeof req.body.month != 'number' || req.body.month < MIN_MONTH || req.body.month > MAX_MONTH) {
+				errors.push(req.app.locals.t['ERRORS']['MONTH_ERROR_FORM']);
+			} 
+			// titre n'existe pas ou n'a pas au moins 5 caractères
+			if (req.body.title === undefined || req.body.title.length < TITLE_MIN_CHARACTER_NUMBER) {
+				errors.push(req.app.locals.t['ERRORS']['PUB_AT_LEAST_5_CHAR_FORM']);
+			} 
+			// Conférence n'existe pas ou n'a pas au moins 5 caractères
+			if (req.body.venue === undefined || req.body.venue.length < VENUE_MIN_CHARACTER_NUMBER) {
+				errors.push(req.app.locals.t['ERRORS']['VENUE_AT_LEAST_5_CHAR_FORM']);
+			} 
+		}
 		
 		if (errors.length > 0) {
 			res.status(400).json({ 'errors': errors });
@@ -79,8 +81,8 @@ module.exports = servicePublication => {
 				authors: req.body.authors,
 				venue: req.body.venue
 			};
-	
-			servicePublication.createPublication(publication)((err) => {
+
+			servicePublication.createPublication(publication)((err, pub) => {
 				if (err) {
 					if (req.app.locals.t === undefined &&
 						req.app.locals.t['ERRORS'] === undefined &&
@@ -90,7 +92,7 @@ module.exports = servicePublication => {
 						res.status(500).json({ 'errors': [req.app.locals.t['ERRORS']['PUB_CREATE_ERROR']] });
 					}
 				} else {
-					res.status(201).json('OK');
+					res.status(201).json(pub);
 				}
 			});
 		}
