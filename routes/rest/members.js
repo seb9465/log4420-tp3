a1d40066ab7757e77bc6e3987fac8c21864a98c7
Sebastien Cadorette <cado.94@me.com>
2019-11-19 13:32:33 -0500

    clean clean member route

--- routes/rest/members.js
+++ routes/rest/members.js
@@ -6,15 +6,13 @@ module.exports = serviceTeam => {
 	router.get('/', (req, res, next) => {
 		serviceTeam.getTeamMembers((err, data) => {
 			if (err) {
-				const istTranslationNotOk = req.app.locals.t === undefined ||
+				const istTranslationNotOk =
+					req.app.locals.t === undefined ||
 					req.app.locals.t['ERRORS'] === undefined ||
 					req.app.locals.t['ERRORS']['MEMBERS_ERROR'] === undefined;
-				
-				if (istTranslationNotOk) {
-					res.status(500).json({ 'errors': [err.message] });
-				} else {
-					res.status(500).json({ 'errors': [req.app.locals.t['ERRORS']['MEMBERS_ERROR']] });
-				}
+				const errorJson = { 'errors': istTranslationNotOk ? [err.message] : [req.app.locals.t['ERRORS']['MEMBERS_ERROR']] };
+
+				res.status(500).json(errorJson);
 			} else {
 				res.json(data);
 			}
