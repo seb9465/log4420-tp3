2e403ff0d59a8e1831236226d0ea4f403b7dadd2
Sebastien Cadorette <cado.94@me.com>
2019-11-19 13:32:15 -0500

    format document

--- routes/rest/feed.js
+++ routes/rest/feed.js
@@ -6,11 +6,10 @@ module.exports = serviceFeed => {
 	router.get('/', (req, res, next) => {
 		serviceFeed.getFeeds(new Date(2019, 09))(req.app.locals.lang)((err, feeds) => {
 			if (err) {
-				const isTranslationNotOk = 
+				const isTranslationNotOk =
 					req.app.locals.t === undefined ||
 					req.app.locals.t['ERRORS'] === undefined ||
 					req.app.locals.t['ERRORS']['FEEDS_ERROR'] === undefined;
-				
 				const errorJson = { 'errors': isTranslationNotOk ? [err.message] : [req.app.locals.t['ERRORS']['FEEDS_ERROR']] };
 
 				res.status(500).json(errorJson);
